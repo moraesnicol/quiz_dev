11aad8c1562ada69d2c4c0c088f67b7937ac6b40
Gabriel Nicol <moraesnicol@gmail.com>
2021-04-19 12:57:39 -0300

    assets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_dev/app_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: AppWidget(),
     );
   }
 }
