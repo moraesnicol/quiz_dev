a3f62086ea4fd884bff3e47fe6458fb3e647bedb
Gabriel Nicol <moraesnicol@gmail.com>
2021-04-19 19:24:59 -0300

    finish app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:dev_quiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
-import 'core/app_widget.dart';
 
 void main() {
   runApp(MyApp());
--- lib/widgets/appbar/app_bar_widiget.dart
+++ lib/widgets/appbar/app_bar_widiget.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dev_quiz/core/app_gradients.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/widgets/score_card/score_card_widiget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidiget extends PreferredSize {
@@ -10,33 +11,40 @@ class AppBarWidiget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(gradient: AppGradients.linear),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(TextSpan(
-                    text: "Olá, ",
-                    style: AppTextStyles.title,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      TextSpan(
-                        text: "Gabriel Nicol",
-                        style: AppTextStyles.titleBold,
-                      )
+                      Text.rich(TextSpan(
+                        text: "Olá, ",
+                        style: AppTextStyles.title,
+                        children: [
+                          TextSpan(
+                            text: "Gabriel Nicol",
+                            style: AppTextStyles.titleBold,
+                          )
+                        ],
+                      )),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://avatars.githubusercontent.com/u/14365242?s=400&u=3911ce08cbf0709c0b6a967835db4d9b62a72a8f&v=4'))),
+                      ),
                     ],
-                  )),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                'https://avatars.githubusercontent.com/u/14365242?s=400&u=3911ce08cbf0709c0b6a967835db4d9b62a72a8f&v=4'))),
                   ),
-                ],
-              ),
+                ),
+                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
+              ],
             ),
           ),
         );
